e127bbf2645a3c5d36a4b616f3e5d40fe8c81c09
Fawool <LMLSDSG@hotmail.com>
2023-01-05 00:02:22 +0800

    Finish PID part

--- include/hero_chassis_controller/hero_chassis_controller.h
+++ include/hero_chassis_controller/hero_chassis_controller.h
@@ -2,18 +2,26 @@
 // Created by qiayuan on 2/6/21.
 //
 
-#ifndef SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
-#define SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
+#ifndef SIMPLE_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
+#define SIMPLE_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
 
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <control_toolbox/pid.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <control_msgs/JointControllerState.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_listener.h>
 
-namespace simple_chassis_controller {
+namespace hero_chassis_controller {
 
-class SimpleChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
+class HeroChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
  public:
-  SimpleChassisController() = default;
-  ~SimpleChassisController() override = default;
+  HeroChassisController() = default;
+  ~HeroChassisController() override = default;
 
   bool init(hardware_interface::EffortJointInterface *effort_joint_interface,
             ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
@@ -22,9 +30,28 @@ class SimpleChassisController : public controller_interface::Controller<hardware
 
   hardware_interface::JointHandle front_left_joint_, front_right_joint_, back_left_joint_, back_right_joint_;
  private:
-  int state_{};
-  ros::Time last_change_;
+ control_toolbox::Pid pid_controller1_;   //left_front
+ control_toolbox::Pid pid_controller2_;   //right_front
+ control_toolbox::Pid pid_controller3_;   //left_back
+ control_toolbox::Pid pid_controller4_;   //right_back
+
+ void get_vel_cmd(const geometry_msgs::TwistConstPtr &msg);
+
+ void compute_the_cmd(ros::NodeHandle& controller_nh);
+
+ double vel_cmd[5]{0.0,0.0,0.0,0.0,0.0};
+ double vel_cur[5]{};
+ double error[5]{0.0,0.0,0.0,0.0};
+    std::unique_ptr<
+            realtime_tools::RealtimePublisher<
+                    control_msgs::JointControllerState> > controller_state_publisher_ ;
+    double Vxe{0.0}, Vye{0.0}, yawe{0.0};
+    double Vxc{},Vyc{},yawc{};
+    double a{0.320},b{0.410},r{0.02};
+
+    ros::Subscriber sub_command_;
+
 };
 }// namespace simple_chassis_controller
 
-#endif //SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
+#endif //HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
--- src/hero_chassis_controller.cpp
+++ src/hero_chassis_controller.cpp
@@ -5,8 +5,8 @@
 #include "hero_chassis_controller/hero_chassis_controller.h"
 #include <pluginlib/class_list_macros.hpp>
 
-namespace simple_chassis_controller {
-bool SimpleChassisController::init(hardware_interface::EffortJointInterface *effort_joint_interface,
+namespace hero_chassis_controller {
+bool HeroChassisController::init(hardware_interface::EffortJointInterface *effort_joint_interface,
                                    ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) {
   front_left_joint_ =
       effort_joint_interface->getHandle("left_front_wheel_joint");
@@ -16,29 +16,49 @@ bool SimpleChassisController::init(hardware_interface::EffortJointInterface *eff
   back_right_joint_ =
       effort_joint_interface->getHandle("right_back_wheel_joint");
 
+  pid_controller1_.init(ros::NodeHandle(controller_nh,"pid1"));
+    pid_controller2_.init(ros::NodeHandle(controller_nh,"pid2"));
+    pid_controller3_.init(ros::NodeHandle(controller_nh,"pid3"));
+    pid_controller4_.init(ros::NodeHandle(controller_nh,"pid4"));
+
+    sub_command_ = root_nh.subscribe<geometry_msgs::Twist>("cmd_vel", 1, &HeroChassisController::get_vel_cmd,this);
+
   return true;
 }
 
-void SimpleChassisController::update(const ros::Time &time, const ros::Duration &period) {
-  double tau = 0.2;  // torque
-  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
-  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
-  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
-  static double cmd_[6][4] = {{tau, tau, tau, tau}, //  forward
-                              {-2 * tau, -2 * tau, -2 * tau, -2 * tau}, //  backward
-                              {-tau, tau, tau, -tau}, //  left
-                              {2 * tau, -2 * tau, -2 * tau, 2 * tau}, //  right
-                              {2 * tau, -2 * tau, 2 * tau, -2 * tau}, //  clockwise
-                              {-tau, tau, -tau, tau}};  //  counterclockwise
-  if ((time - last_change_).toSec() > 2) {
-    state_ = (state_ + 1) % 6;
-    last_change_ = time;
-  }
-  front_left_joint_.setCommand(cmd_[state_][0]);
-  front_right_joint_.setCommand(cmd_[state_][1]);
-  back_left_joint_.setCommand(cmd_[state_][2]);
-  back_right_joint_.setCommand(cmd_[state_][3]);
+void HeroChassisController::get_vel_cmd(const geometry_msgs::TwistConstPtr &msg) {
+    Vxe=msg->linear.x;
+    Vye=msg->linear.y;
+    yawe=msg->angular.z;
 }
 
-PLUGINLIB_EXPORT_CLASS(simple_chassis_controller::SimpleChassisController, controller_interface::ControllerBase)
+    void HeroChassisController::compute_the_cmd(ros::NodeHandle &controller_nh) {
+        vel_cmd[1] = (Vxe + Vye + yawe * (a + b) / 2) / r;
+        vel_cmd[2] = (Vxe - Vye - yawe * (a + b) / 2) / r;
+        vel_cmd[3] = (Vxe + Vye - yawe * (a + b) / 2) / r;
+        vel_cmd[4] = (Vxe - Vye + yawe * (a + b) / 2) / r;
+};
+
+
+
+    void HeroChassisController::update(const ros::Time &time, const ros::Duration &period) {
+
+    vel_cur[1]=front_right_joint_.getVelocity();
+    vel_cur[2]=front_left_joint_.getVelocity();
+    vel_cur[3]=back_left_joint_.getVelocity();
+    vel_cur[4]=back_right_joint_.getVelocity();
+
+    error[1]=vel_cmd[1]-vel_cur[1];
+    error[2]=vel_cmd[2]-vel_cur[2];
+    error[3]=vel_cmd[3]-vel_cur[3];
+    error[4]=vel_cmd[4]-vel_cur[4];
+
+
+    front_right_joint_.setCommand(pid_controller1_.computeCommand(error[1], period));
+  front_left_joint_.setCommand(pid_controller2_.computeCommand(error[2], period));
+  back_left_joint_.setCommand(pid_controller3_.computeCommand(error[3], period));
+  back_right_joint_.setCommand(pid_controller4_.computeCommand(error[4], period));
+}
+
+PLUGINLIB_EXPORT_CLASS(hero_chassis_controller::HeroChassisController, controller_interface::ControllerBase)
 }
